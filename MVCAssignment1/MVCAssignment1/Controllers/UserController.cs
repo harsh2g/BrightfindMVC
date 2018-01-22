2c9b559f30efeb160540567516cc8273aeb9a2c6
harsh2g <harshgadani55@gmail.com>
2018-01-22 15:07:52 -0500

    MVC Demo

---
+++ MVCAssignment1/MVCAssignment1/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using MVCAssignment1.Models;
+using System.Web.Mvc;
+using Core.Calendar;
+
+namespace MVCAssignment1.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+
+        public ActionResult Index()
+        {
+            var users = new UserModel();
+            var list = users.GetList();
+            return View(list);
+        }
+        public ActionResult Edit(int id)
+        {
+            var ctx = new UserModel();
+            var user = ctx.GetUser(id);
+            return View(user);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Edit(User u)
+        {
+            var ctx = new UserModel();
+            ctx.Update(u);
+            return RedirectToAction("Index");
+        }
+
+        
+        public ActionResult Delete(int id)
+        {
+            var ctx = new UserModel();
+            ctx.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Add(User u)
+        {
+            var ctx = new UserModel();
+            ctx.Add(u);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SessionTest()
+        {
+            HttpContext.Session["SessionName"] = "mySession";
+            return View();
+        }
+    }
+}
---
+++ MVCAssignment1/MVCAssignment1/Models/User.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCAssignment1.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string University { get; set; }
+        public string Gender { get; set; }
+        public int ID { get; set; }
+    }
+
+    public class UserModel
+    {
+        public static List<User> userList = new List<User>
+        {
+            new User {
+                FirstName = "Harsh",
+                LastName = "Gadani",
+                University = "Salisbury University",
+                Gender = "Male",
+                ID = 1
+            }
+        };
+
+        public UserModel()
+        {
+
+        }
+
+        public void Update(User u)
+        {
+            User update = userList.Find(user => user.ID == u.ID);
+            update.FirstName = u.FirstName;
+            update.LastName = u.LastName;
+            update.University = u.University;
+            update.Gender = u.Gender;
+        }
+
+        
+
+        public void Add(User u)
+        {
+            u.ID = userList.Count() + 1;
+            userList.Add(u);
+        }
+
+        public List<User> GetList()
+        {
+            return userList;
+        }
+
+        public void Delete(int id)
+        {
+            userList.RemoveAll(u => u.ID == id);
+        }
+
+        public User GetUser(int id)
+        {
+            return userList.Find(u => u.ID == id);
+        }        
+    }
+}
